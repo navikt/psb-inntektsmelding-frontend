0cbbccb6431ea7f69d549f99893327997c9cec5a
Hallvard Andreas Stark <hallvard@scel.to>
2021-05-10 15:28:38 +0200

    la inn box-komponenten og litt styling

--- src/ui/MainComponent.tsx
+++ src/ui/MainComponent.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
 import { Period } from '@navikt/period-utils';
+import React from 'react';
 import { Kompletthet as KompletthetsData } from '../types/KompletthetData';
 import { Kompletthet as KompletthetResponse } from '../types/KompletthetResponse';
+import Box, { Margin } from './components/box/Box';
 
 interface MainComponentProps {
     response: KompletthetResponse;
@@ -23,25 +24,37 @@ const MainComponent = ({ response }: MainComponentProps) => {
     const { tilstand } = initKompletthetsdata(response);
     return (
         <div className="kompletthet">
-            <ul>
-                {tilstand.map((kompletthetstilstand) => {
-                    const { periode } = kompletthetstilstand;
-                    return (
-                        <li key={JSON.stringify(periode)}>
-                            <p>{periode.prettifyPeriod()}</p>
-                            {kompletthetstilstand.status.map(({ arbeidsgiver, journalpostId, status }) => {
-                                return (
-                                    <p key={journalpostId}>
-                                        {JSON.stringify(arbeidsgiver)}
-                                        {status}
-                                        {journalpostId}
-                                    </p>
-                                );
-                            })}
-                        </li>
-                    );
-                })}
-            </ul>
+            <h1 style={{ fontSize: 22 }}>Kompletthet</h1>
+            <Box marginTop={Margin.xLarge}>
+                <ul>
+                    {tilstand.map((kompletthetstilstand) => {
+                        const { periode } = kompletthetstilstand;
+                        return (
+                            <li key={JSON.stringify(periode)} style={{ marginBottom: '3rem' }}>
+                                <p>{periode.prettifyPeriod()}</p>
+                                <div style={{ display: 'flex' }}>
+                                    {kompletthetstilstand.status.map(({ arbeidsgiver, journalpostId, status }) => {
+                                        return (
+                                            <div
+                                                key={journalpostId}
+                                                style={{
+                                                    display: 'flex',
+                                                    flexDirection: 'column',
+                                                    marginRight: '4rem',
+                                                }}
+                                            >
+                                                <p>Arbeidsgiver: {arbeidsgiver.arbeidsgiver}</p>
+                                                <p>Status: {status}</p>
+                                                <p>JournalpostId: {journalpostId}</p>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </Box>
         </div>
     );
 };
