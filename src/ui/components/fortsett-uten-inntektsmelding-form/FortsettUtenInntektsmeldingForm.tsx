268638c774af292385b77e49c836c701aa3b0f9b
Hallvard Andreas Stark <hallvard@scel.to>
2021-05-14 13:15:39 +0200

    litt styling tilpasset ny versjon av react-hook-form

--- src/ui/components/fortsett-uten-inntektsmelding-form/FortsettUtenInntektsmeldingForm.tsx
+++ src/ui/components/fortsett-uten-inntektsmelding-form/FortsettUtenInntektsmeldingForm.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import { Hovedknapp } from 'nav-frontend-knapper';
-import { useForm } from 'react-hook-form';
-import TextArea from '../../form/TextArea';
 import { Checkbox } from 'nav-frontend-skjema';
+import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import TextArea from '../../form/TextArea';
+import Box, { Margin } from '../box/Box';
 
 interface FortsettUtenInntektsmeldingFormState {
     begrunnelse: string;
@@ -13,19 +14,33 @@ interface FortsettUtenInntektsmeldingFormProps {
 }
 
 const FortsettUtenInntektsmeldingForm = ({ onSubmit }: FortsettUtenInntektsmeldingFormProps) => {
-    const { handleSubmit, watch } = useForm({});
-    const fortsettUtenInntektsmelding = watch('fortsettUtenInntektsmelding');
+    const formMethods = useForm({});
+    const { handleSubmit, watch, register } = formMethods;
+    const fortsettUtenInntektsmelding = watch('fortsettUtenInntektsmelding', false);
 
     return (
-        <form onSubmit={() => handleSubmit(onSubmit)}>
-            <Checkbox name="fortsettUtenInntektsmelding" label="Fortsett uten inntektsmelding" />
-            <TextArea
-                name="begrunnelse"
-                label="Begrunnelse"
-                validators={{ something: (v) => (!v ? 'Du må fylle inn en verdi' : null) }}
-            />
-            <Hovedknapp disabled={!fortsettUtenInntektsmelding}>Lagre</Hovedknapp>
-        </form>
+        <FormProvider {...formMethods}>
+            <form onSubmit={() => handleSubmit(onSubmit)}>
+                <Checkbox
+                    name="fortsettUtenInntektsmelding"
+                    label="Fortsett uten inntektsmelding"
+                    {...register('fortsettUtenInntektsmelding')}
+                />
+
+                {fortsettUtenInntektsmelding && (
+                    <Box marginTop={Margin.medium}>
+                        <TextArea
+                            name="begrunnelse"
+                            label="Begrunnelse"
+                            validators={{ something: (v) => (!v ? 'Du må fylle inn en verdi' : null) }}
+                        />
+                    </Box>
+                )}
+                <Box marginTop={Margin.large}>
+                    <Hovedknapp disabled={!fortsettUtenInntektsmelding}>Lagre</Hovedknapp>
+                </Box>
+            </form>
+        </FormProvider>
     );
 };
 
