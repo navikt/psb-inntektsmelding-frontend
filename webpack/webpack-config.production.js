6d09eb09a84e5e469d17f9cc7a348b30dd1591cc
Erlend <erlend.edvardsen.vige@nav.no>
2021-06-10 15:35:36 +0200

    Update build/deploy

--- webpack/webpack-config.production.js
+++ webpack/webpack-config.production.js
@@ -10,7 +10,7 @@ module.exports = merge(commonWebpackConfig, {
     mode: 'production',
     output: {
         filename: 'app.js',
-        path: path.resolve(__dirname, `../build/${pkg.version}`),
+        path: path.resolve(__dirname, '../build'),
     },
     optimization: {
         minimize: true,
